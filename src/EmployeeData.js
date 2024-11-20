eb95285e2ee020d2ac316d62f01d9d40582ff9b8
Indranil102 <indranilsamanta2003@gmail.com>
2024-11-20 19:27:04 +0530

    Normalize line endings

---
+++ src/EmployeeData.js
@@ -0,0 +1,23 @@
+export const EmployeeData =[
+    {
+        id:1,
+        firstName : 'Mark',
+        lastName :'David',
+        age:20
+
+    },
+    {
+        id:2,
+        firstName : 'Indranil ',
+        lastName :'Samanta',
+        age:21
+
+    },
+    {
+        id:3,
+        firstName : 'Iam ',
+        lastName :'Someone',
+        age:21
+
+    },
+]
